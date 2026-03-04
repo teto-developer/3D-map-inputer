901af3ce0b507cd5cb751043bb66b85d7762067e
teto <ryouseikoseki1114@gmail.com>
2026-03-04 12:40:39 +0900

    Update server.py

--- server.py
+++ server.py
@@ -7,7 +7,6 @@ app = Flask(__name__)
 ZIP_URL = "https://gic-plateau.s3.ap-northeast-1.amazonaws.com/2023/13999_tokyo_udx-mlit_2023_3dtiles_mtv_1_sample-takeshiba_op.zip"
 ZIP_PATH = "sample.zip"
 
-# 起動時にダウンロード（なければ）
 if not os.path.exists(ZIP_PATH):
     print("Downloading ZIP...")
     r = requests.get(ZIP_URL, stream=True)
@@ -25,4 +24,5 @@ def download_zip():
     return send_file(ZIP_PATH, as_attachment=True)
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=10000)
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host="0.0.0.0", port=port)
